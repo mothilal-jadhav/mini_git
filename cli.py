967eabee8d2f6d608086814d0ca3dba21b6a2a3c
Mothilal Jadhav <mothilaljadav0304@gmail.com>
2026-06-10 20:12:44 +0530

    added add functionality

--- cli.py
+++ cli.py
@@ -1,9 +1,15 @@
 # cli.py
 
 import sys
+
 from mini_git.repository import init_repo
+from mini_git.repository import add_file
 
 command = sys.argv[1]
 
 if command == "init":
     init_repo()
+
+elif command == "add":
+    filepath = sys.argv[2]
+    add_file(filepath)
