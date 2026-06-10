bf40aa9ed39636a7a85a1eea280395dd27bfa546
Mothilal Jadhav <mothilaljadav0304@gmail.com>
2026-06-10 20:11:39 +0530

    modified init_repo

--- mini_git/repository.py
+++ mini_git/repository.py
@@ -1,8 +1,32 @@
 # mini_git/repository.py
-
+from mini_git.objects import store_blob
+import json
 import os
 
 GIT_DIR = ".mini_git"
+INDEX_FILE = os.path.join(GIT_DIR, "index")
+
+def add_file(filepath):
+
+    sha = store_blob(filepath)
+
+    if os.path.exists(INDEX_FILE):
+        with open(INDEX_FILE, "r") as f:
+            content = f.read()
+
+            if content:
+                index = json.loads(content)
+            else:
+                index = {}
+    else:
+        index = {}
+
+    index[filepath] = sha
+
+    with open(INDEX_FILE, "w") as f:
+        json.dump(index, f, indent=4)
+
+    print(f"Added {filepath}")
 
 def init_repo():
     if os.path.exists(GIT_DIR):
